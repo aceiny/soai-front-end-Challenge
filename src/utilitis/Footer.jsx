9652a714315f2ab523c7438f0809a3387cc97a28
aceiny <a_zeraibi@estin.dz>
2023-12-26 07:01:36 +0100

    minor changes

--- src/utilitis/Footer.jsx
+++ src/utilitis/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../assets/logo.svg'
+import logo from '../assets/LOGO.svg'
 import instagram from '../assets/instagram.svg'
 import linkedin from '../assets/linkedin.svg'
 const Footer = () => {
